54ba8c987834f240a65a881b55e4dc2df953083f
Мокрецова Полина Максимовна <mokretsova@tochka.com>
2022-10-13 09:03:04 +0500

    edit config

--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -9,12 +9,12 @@ from util.attachment import add_video
 
 @allure.tag('mobile')
 @allure.title('Test search')
-def test_wiki_browserstack(app_android):
+def test_wiki_search(app_android):
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('BrowserStack')
     with step('Verify content found'):
-        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))\
+        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')) \
             .should(have.size_greater_than(0))
     add_video(browser)
 
@@ -31,3 +31,35 @@ def test_wiki_sqa(app_android):
         ).should(have.size_greater_than(0))
     add_video(browser)
 
+
+def test_wiki_appium(app_android):
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Appium")
+    with step('Verify content found'):
+        browser.all(
+            (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
+        ).should(have.size_greater_than(0))
+    add_video(browser)
+
+
+def test_wiki_api_search(app_android):
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Api")
+    with step('Verify content found'):
+        browser.all(
+            (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
+        ).should(have.size_greater_than(0))
+    add_video(browser)
+
+
+def test_wiki_selenium_search(app_android):
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Selenium")
+    with step('Verify content found'):
+        browser.all(
+            (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
+        ).should(have.size_greater_than(0))
+    add_video(browser)
